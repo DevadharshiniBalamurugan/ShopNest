268b9165af96a03aa60e9e5107c45d7222352677
DevadharshiniBalamurugan <devadharshinibalamurugan49@gmail.com>
2026-09-16 16:33:24 +0530

    Seed empty catalog on application startup

--- shopnest/wsgi.py
+++ shopnest/wsgi.py
@@ -1,4 +1,13 @@
 import os
+from django.core.management import call_command
+from django.db import OperationalError, ProgrammingError
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','shopnest.settings')
 application=get_wsgi_application()
+
+try:
+	from store.models import Product
+	if not Product.objects.exists():
+		call_command('seed_data', verbosity=0)
+except (OperationalError, ProgrammingError):
+	pass
